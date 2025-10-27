501070ad3e472f2dccdf794e28aee49d635be7b4
47 <usmanmurtzaa47@gmail.com>
2025-10-27 18:17:08 +0530

    Update script.js

--- Da-Social-Network/script.js
+++ Da-Social-Network/script.js
@@ -2,16 +2,37 @@ document.querySelector('.button1').addEventListener('click',function(){
 document.querySelector('.createapost').classList.add('show')
 })
 
+document.querySelector('.editprofile1').addEventListener('click',function(){
+document.querySelector('.createapost1').classList.add('show')
+document.querySelector('.helpdude1').classList.remove('show');
+})
+
 document.querySelector('.post69').addEventListener('click',function(e){
   e.preventDefault();
 document.querySelector('.createapost').classList.remove('show')
+})
+
+document.querySelector('.post70').addEventListener('click',function(e){
+  e.preventDefault();
+document.querySelector('.createapost1').classList.remove('show');
 })
 
 document.querySelector('.fixedbtn').addEventListener('click',function(){
   document.querySelector('.helpdude1').classList.toggle('show')
 })
 
-
-document.querySelector('.deletebtn').addEventListener('click',function(){
-document.querySelector('.message2').classList.add('hide')
+// COUNTER
+let countlikes=0;
+let countdislikes=0;
+document.querySelector('.likes1').addEventListener('click',function(){
+  countlikes=countlikes+1;
+  document.querySelector(".counter").textContent=countlikes;
 })
+
+document.querySelector('.dislikes1').addEventListener('click',function(){
+countdislikes=countlikes+1;
+document.querySelector(".counter").textContent=countdislikes;
+})
+
+
+
